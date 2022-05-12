6642b30f67dd6170d9bb4b8ec0a87c02e9bf9537
ᴠᴇxɪᴅᴏ <103483308+vexido@users.noreply.github.com>
2022-05-12 18:23:48 +0700

    make simple afk timer

--- EmikoRobot/modules/sql/afk_sql.py
+++ EmikoRobot/modules/sql/afk_sql.py
@@ -1,7 +1,8 @@
 
 import threading
 
-from sqlalchemy import Boolean, Column, BigInteger, UnicodeText
+from sqlalchemy import Boolean, Column, BigInteger, UnicodeText, DateTime
+from datetime import datetime
 
 from EmikoRobot.modules.sql import BASE, SESSION
 
@@ -12,11 +13,13 @@ class AFK(BASE):
     user_id = Column(BigInteger, primary_key=True)
     is_afk = Column(Boolean)
     reason = Column(UnicodeText)
+    time = Column(DateTime)
 
-    def __init__(self, user_id, reason="", is_afk=True):
+    def __init__(self, user_id: int, reason: str = "", is_afk: bool = True):
         self.user_id = user_id
         self.reason = reason
         self.is_afk = is_afk
+        self.time = datetime.now()
 
     def __repr__(self):
         return "afk_status for {}".format(self.user_id)
@@ -47,7 +50,7 @@ def set_afk(user_id, reason=""):
         else:
             curr.is_afk = True
 
-        AFK_USERS[user_id] = reason
+        AFK_USERS[user_id] = {"reason": reason, "time": curr.time}
 
         SESSION.add(curr)
         SESSION.commit()
@@ -85,7 +88,9 @@ def __load_afk_users():
     global AFK_USERS
     try:
         all_afk = SESSION.query(AFK).all()
-        AFK_USERS = {user.user_id: user.reason for user in all_afk if user.is_afk}
+        AFK_USERS = {
+            user.user_id: {"reason": user.reason, "time": user.time} for user in all_afk if user.is_afk
+        }
     finally:
         SESSION.close()
 
